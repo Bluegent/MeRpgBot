66e6b02a9019f9e13424ecab87a9856cdde1a9f0
alexionita95 <ionita.alexandru95@gmail.com>
2019-07-31 15:56:50 +0300

    Added final classes for JSON parsing.

---
+++ MicroExpressionParser/DataCollection.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace MicroExpressionParser
+{
+    public partial class DataCollection
+    {
+        [JsonProperty("base_value_keys")]
+        [DefaultValue(null)]
+        public List<string> BaseValueKeys { get; set; }
+
+        [JsonProperty("attributes_keys")]
+        [DefaultValue(null)]
+        public List<string> AttributesKeys { get; set; }
+
+        [JsonProperty("damage_types")]
+        [DefaultValue(null)]
+        public List<DamageType> DamageTypes { get; set; }
+
+        [JsonProperty("compound_stats")]
+        [DefaultValue(null)]
+        public List<CompoundStat> CompundStats { get; set; }
+
+        [JsonProperty("entity_resource")]
+        [DefaultValue(null)]
+        public List<CompoundStat> EntityResource { get; set; }
+
+        [JsonProperty("status_effects")]
+        [DefaultValue(null)]
+        public List<StatusEffect> StatusEffects { get; set; }
+
+        [JsonProperty("skills")]
+        [DefaultValue(null)]
+        public List<Skill> Skills { get; set; }
+
+        [JsonProperty("classes")]
+        [DefaultValue(null)]
+        public List<Class> Classes { get; set; }
+
+        public static DataCollection FromJson(string json)
+        {
+            if (json == null || json == "")
+            {
+                return null;
+            }
+            DataCollection result;
+            try
+            {
+                result = JsonConvert.DeserializeObject<DataCollection>(json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                result = null;
+            }
+            return result;
+        }
+    }
+}
---
+++ MicroExpressionParser/Skill.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+namespace MicroExpressionParser
+{
+    public partial class Skill
+    {
+        [JsonProperty("key")]
+        [DefaultValue(null)]
+        public string Key { get; set; }
+
+        [JsonProperty("name")]
+        [DefaultValue(null)]
+        public string Name { get; set; }
+
+        [JsonProperty("aliases")]
+        [DefaultValue(null)]
+        public List<string> Aliases { get; set; }
+
+        [JsonProperty("description")]
+        [DefaultValue(null)]
+        public string Description { get; set; }
+
+        [JsonProperty("type")]
+        [DefaultValue(null)]
+        public string Type { get; set; }
+
+        [JsonProperty("values_by_level")]
+        [DefaultValue(null)]
+        public List<ValueByLevel> ValuesByLevel { get; set; }
+
+        public static Skill FromJson(string json)
+        {
+            if (json == null || json == "")
+            {
+                return null;
+            }
+            Skill result;
+            try
+            {
+                result = JsonConvert.DeserializeObject<Skill>(json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                result = null;
+            }
+            return result;
+        }
+    }
+}
