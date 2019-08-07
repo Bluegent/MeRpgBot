1ec22381947a3dc59307eb8054dafc894aaefeeb
alexionita95 <ionita.alexandru95@gmail.com>
2019-08-07 16:06:02 +0300

    Added JsonLoader and DataManager classes.

---
+++ MicroExpressionParser/Data/JSONLoader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace EngineTest
+{
+    using Newtonsoft.Json;
+
+    public class JsonLoader
+    {
+
+        public static JObject LoadFromFile(string path)
+        {
+            JObject result = null;
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    if (sr != null)
+                    {
+                        string json = sr.ReadToEnd();
+                        result = JsonConvert.DeserializeObject<JObject>(json);
+                        sr.Close();
+
+                    }
+                }
+
+                
+            }
+            return result;
+
+        }
+    }
+}
