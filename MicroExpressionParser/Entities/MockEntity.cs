b59399d7088205908d6cbb662319b0313ec1c9d1
Bluegent <bluegentleman@live.com>
2019-08-23 19:04:48 +0300

    Split Entity file into multiple classes

---
+++ MicroExpressionParser/Entities/MockEntity.cs
@@ -0,0 +1,27 @@
+﻿using RPGEngine.Core;
+using RPGEngine.Parser;
+
+namespace RPGEngine.Entities
+{
+
+    public class MockEntity : BaseEntity
+    {
+        public MockEntity(IGameEngine engine)
+            : base(engine)
+        {
+            AddAttribute("CHP", 100);
+            AddAttribute("MHP", 100);
+            AddAttribute("STR", 5);
+            AddAttribute("INT", 5);
+            AddAttribute("AGI", 10);
+            AddAttribute("DEF", 10);
+            AddAttribute("MDEF", 10);
+
+            MeNode mpNode = TreeConverter.Build("INT*10", engine);
+            MeNode mpRegen = TreeConverter.Build("INT/100", engine);
+            ResourceTemplate resTemp = new ResourceTemplate("MP", mpNode, mpRegen);
+            AddResource(resTemp);
+
+        }
+    }
+}
